2bdf1ffc9271a705192f25c84a1dab52d9fafb64
Dusty <deltac.sierra@gmail.com>
2019-03-11 03:54:33 -0400

    Update JCF-QCB.js
    Truncated

--- JCF-QCB.js
+++ JCF-QCB.js
@@ -1,11 +1,11 @@
     // Quote and code copy/collapse functionality. Support: https://forum.jcink.com/index.php?showtopic=27980
     document.addEventListener('DOMContentLoaded', function() {
-		let qcLi = document.querySelectorAll("[id='CODE-WRAP'], [id='QUOTE-WRAP']")
+		let qcLi = document.querySelectorAll("#CODE-WRAP, #QUOTE-WRAP")
 		for (var i=0;i<qcLi.length;i++) {
 		  let w = qcLi[i];
 		  let c = w.querySelector('td[id]');
 		  w.classList.add(w.getAttribute('id'))
-		  c.classList.add(c.getAttribute('id'))
+		  w.querySelector('td[id]').classList.add(c.getAttribute('id'))
 		  c.innerHTML = '<div class="collapsible">'+c.innerHTML+'</div>'
 		  w.querySelector('td:first-of-type').innerHTML += '<span class="qcLi-select">(Copy to clipboard)</span><span class="qcLi-collapse-btn"></span>'
 		  w.removeAttribute('id')
